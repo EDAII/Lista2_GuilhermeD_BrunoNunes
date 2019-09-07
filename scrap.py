c72a3c12b7f4e7534ddc014278c3261c92d8d0bc
Guilherme Dourado <guibdourado@gmail.com>
2019-09-07 16:34:21 -0300

    feat: generated csv file with html content

--- scrap.py
+++ scrap.py
@@ -1,9 +1,16 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+
+import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+
+
+def dict_to_csv(filename, disciplines):
+    with open(filename, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Nome', 'Codigo'])
+        for discipline in disciplines:
+                writer.writerow([discipline['Nome'], discipline['Codigo']])
+
 
 # url da pagina do curriculo de eng. de software
 url = "https://matriculaweb.unb.br/graduacao/curriculo.aspx?cod=6360"
@@ -18,32 +25,30 @@ page_soup = soup(html, "html.parser")
 table_list = page_soup.findAll("table", {"id": "datatable"})
 
 
-print(len(table_list))
 table_list.remove((table_list[0]))
 
-row_list = []
+
 discipline_code_list = []
 discipline_names = []
+
+#extraindo do html os codigos das disciplinas
 for i in range(len(table_list)):
     for table_row in table_list:
-        tr = table_row.find("tr")
-        row_list.append(tr)
+        td = table_row.find("td").text
+        discipline_code_list.append(td)
 
-
-for i in range(len(row_list)):
+#extraindo do html os nomes das disciplinas
+for i in range(len(table_list)):
     for discipline in table_list:
-        discipline_name = discipline.find("a")
-        discipline_names.append(discipline_name)
+        discipline_name = discipline.find("a").text
+        discipline_names.append(" ".join(discipline_name.split()))
+
+disciplines = []
+for i in range(len(discipline_names)):
+    temp = {}
+    temp['Nome'] = discipline_names[i]
+    temp['Codigo'] = discipline_code_list[i]
+    disciplines.append(temp)
 
 
-
-
-
-
-print("Table List " + str(len(table_list)) + str(table_list))
-print("\n")
-print("Row List " + str(len(row_list)) + str(row_list) )
-print("\n")
-print("Discipline name list " + str(len(discipline_names)) + str(discipline_names))
-
-
+dict_to_csv('disciplines.csv', disciplines)
